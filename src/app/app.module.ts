3d018dd8b52472aa6b9105da21af0a6e8ea794c8
Danniel Oliveira <dannielgd@gmail.com>
2022-11-25 12:00:32 -0300

    traduzindo as datas e o valor do lançamento e o cadastro de lançamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentoService } from './lancamentos/lancamento.service';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-
+registerLocaleData(localePt);
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +32,13 @@ import { LancamentoService } from './lancamentos/lancamento.service';
 
     ToastModule,
     ConfirmDialogModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+      }),
 
     LancamentosModule,
     PessoasModule,
@@ -32,7 +46,14 @@ import { LancamentoService } from './lancamentos/lancamento.service';
     CoreModule,
     HttpClientModule
   ],
-  providers: [MessageService, ConfirmationService, LancamentoService],
+  providers: [
+    MessageService,
+    ConfirmationService,
+    LancamentoService,
+    TranslateService,
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
