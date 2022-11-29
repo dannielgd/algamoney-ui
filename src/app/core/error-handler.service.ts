4aba0ac6c1002ed98363f8236fb4920778c852c6
Danniel Oliveira <dannielgd@gmail.com>
2022-11-29 14:54:53 -0300

    Mostrando mensagem de erro na tela

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { MessageService } from 'primeng/api';
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,21 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
-    } else {
+    } else if (errorResponse instanceof HttpErrorResponse
+      && errorResponse.status >= 400 && errorResponse.status <= 499) {
+    msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+    try {
+      msg = errorResponse.error[0].mensagemUsuario;
+    } catch (e) { }
+
+    console.error('Ocorreu um erro', errorResponse);
+
+  } else{
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
+      console.error(typeof errorResponse);
+
     }
 
     this.messageService.add({ severity: 'error', detail: msg });
